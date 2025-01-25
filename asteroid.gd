98aea06bb0afdee74c9dae2ba590576cd7b2ae6e
AndreSoftwareDeveloper <andriej2301@gmail.com>
2025-01-25 21:43:48 +0100

    feat: Removing an asteroid after contact with the player

--- asteroid.gd
+++ asteroid.gd
@@ -2,13 +2,14 @@ extends Area2D
 
 const SPEED = 300.0
 var always_move_left := true
+@onready var player: CharacterBody2D = $Player
 
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-
-func _on_body_entered(body):
-	print("body entered")
-	if body.is_in_group("player"):
-		queue_free()
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _physics_process(delta: float) -> void:
 	position.x -= SPEED * delta
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		queue_free()
