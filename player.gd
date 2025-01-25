584a3376ce98c879142c03238a896d19baca481e
AndreSoftwareDeveloper <andriej2301@gmail.com>
2025-01-25 15:26:06 +0100

    feat: Inertia when changing the direction of movement

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 270.0
 const DECELERATION = 75.0
+const TURN_DECELERATION = 200.0
 
 func _physics_process(delta: float) -> void:
 	var input_direction := Vector2(
@@ -11,7 +12,11 @@ func _physics_process(delta: float) -> void:
 
 	if input_direction.length() > 0:
 		input_direction = input_direction.normalized()
-		velocity = input_direction * SPEED
+
+		if velocity.dot(input_direction) < 0:
+			velocity = velocity.move_toward(Vector2.ZERO, TURN_DECELERATION * delta)
+		else:
+			velocity = input_direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
 
