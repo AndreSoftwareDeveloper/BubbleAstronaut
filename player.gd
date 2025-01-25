692ff6ca4da1032a4f7d0abad88395dc345e5660
AndreSoftwareDeveloper <andriej2301@gmail.com>
2025-01-25 17:21:45 +0100

    feat: Object attracting player object

--- player.gd
+++ player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 270.0
-const DECELERATION = 75.0
+const SPEED = 220.0
+const DECELERATION = 130.0
 const TURN_DECELERATION = 200.0
+
+var target_velocity := Vector2.ZERO
+@onready var magnet: CharacterBody2D = $"../Magnet"
 
 func _physics_process(delta: float) -> void:
 	var input_direction := Vector2(
@@ -12,12 +15,18 @@ func _physics_process(delta: float) -> void:
 
 	if input_direction.length() > 0:
 		input_direction = input_direction.normalized()
-
-		if velocity.dot(input_direction) < 0:
-			velocity = velocity.move_toward(Vector2.ZERO, TURN_DECELERATION * delta)
+		
+		if velocity.length() > 0:
+			target_velocity = input_direction * SPEED
+			if velocity.dot(input_direction) < 0:
+				velocity = velocity.move_toward(Vector2.ZERO, TURN_DECELERATION * delta)
 		else:
-			velocity = input_direction * SPEED
+			target_velocity = input_direction * SPEED
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
+		var direction_to_magnet = magnet.position - position
+		direction_to_magnet = direction_to_magnet.normalized()
+		target_velocity = direction_to_magnet * SPEED
+	
+	velocity = velocity.move_toward(target_velocity, DECELERATION * delta)
 
 	move_and_slide()
